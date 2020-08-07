2e34ae4c6face7cab6974f440279392e5aac1c5b
Daniel Meireles <daniel.meireles@outlook.com>
2020-08-06 21:24:12 -0300

    Landing Page development and styling

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -7,10 +7,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 40
   },
+
   banner: {
     width: '100%',
     resizeMode: 'contain'
   },
+
   title: {
     fontFamily: 'Poppins_400Regular',
     color: '#FFF',
@@ -18,8 +20,46 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     marginTop: 80
   },
+
   titleBold: {
     fontFamily: 'Poppins_600SemiBold',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between'
+  },
+
+  button: {
+    height: 150,
+    width: '48%',
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: 'space-between'
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871F5'
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04D361'
+  },
+
+  buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#FFF',
+    fontSize: 20
+  },
+
+  totalConnections: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#D4C2FF',
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+    marginTop: 40
   }
 });
 
